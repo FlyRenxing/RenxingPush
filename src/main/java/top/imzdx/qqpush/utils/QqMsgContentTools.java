38ba00e1f9943a9d06c964baa4874587047bae45
Renxing <1277489864@qq.com>
2021-09-06 23:32:44 +0800

    新增敏感词检测功能

--- src/main/java/top/imzdx/qqpush/utils/QqMsgContentTools.java
+++ src/main/java/top/imzdx/qqpush/utils/QqMsgContentTools.java
@@ -1,10 +1,18 @@
 package top.imzdx.qqpush.utils;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.dfa.WordTree;
 import net.mamoe.mirai.message.code.MiraiCode;
 import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -15,11 +23,21 @@ import java.util.regex.Pattern;
  */
 @Component
 public class QqMsgContentTools {
+    WordTree badWord = new WordTree();
+
     public Message buildMessage(String content) {
+        badWordDFA(content);
         //TODO 加入图片的消息链构建功能
         MessageChainBuilder chainBuilder = new MessageChainBuilder();
         buildFace(content, chainBuilder);
         return chainBuilder.build();
+    }
+
+    public void badWordDFA(String content) {
+        List<String> matchAll = badWord.matchAll(content, -1, false, false);
+        if (matchAll.size()!=0){
+            throw new DefinitionException("消息有敏感词，请检查后再试。提示："+matchAll.toString());
+        }
     }
 
     private void buildFace(String content, MessageChainBuilder chainBuilder) {
@@ -60,5 +78,21 @@ public class QqMsgContentTools {
                 chainBuilder.append(o);
             }
         }
+    }
+
+    public QqMsgContentTools() {
+        URL path = ResourceUtil.getResource("static/敏感词.txt");
+        File file = FileUtil.file(path);// Text文件
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));// 构造一个BufferedReader类来读取文件
+            String s = null;
+            while ((s = br.readLine()) != null) {// 使用readLine方法，一次读一行
+                badWord.addWord(s);
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new DefinitionException("导入敏感词汇失败");
+        }
+
     }
 }
