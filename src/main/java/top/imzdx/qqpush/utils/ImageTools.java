a0c3f0de6df6daf921251c4eb08c0a6a1fd1ce78
Renxing <flyrenxing@qq.com>
2022-05-04 23:29:51 +0800

    完善图片检测机制

--- src/main/java/top/imzdx/qqpush/utils/ImageTools.java
+++ src/main/java/top/imzdx/qqpush/utils/ImageTools.java
@@ -1,16 +1,11 @@
 package top.imzdx.qqpush.utils;
 
+import cn.hutool.core.img.gif.GifDecoder;
+import cn.hutool.core.io.resource.BytesResource;
+import cn.hutool.core.io.resource.MultiResource;
 import cn.hutool.http.HttpRequest;
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.green.model.v20180509.ImageSyncScanRequest;
-import com.aliyuncs.http.FormatType;
-import com.aliyuncs.http.HttpResponse;
-import com.aliyuncs.profile.DefaultProfile;
-import com.aliyuncs.profile.IClientProfile;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
+import cn.hutool.http.HttpUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,125 +13,174 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.*;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ImageTools {
-    private static String accessKeyId;
-    private static String accessKeySecret;
-    private static String useType;
-    private static String imagePrivateCloudUrl;
+    public static final String TYPE_JPG = "jpg";
+    public static final String TYPE_GIF = "gif";
+    public static final String TYPE_PNG = "png";
+    public static final String TYPE_BMP = "bmp";
+    public static final String TYPE_UNKNOWN = "unknown";
+    private final boolean enableCheck;
+    private final String useType;
+    private final String imagePrivateCloudUrl;
+    private AliyunLib aliyunLib;
 
     @Autowired
-    public ImageTools(@Value("${aliyun.accessKeyId}") String accessKeyId,
-                      @Value("${aliyun.accessKeySecret}") String accessKeySecret,
+    public ImageTools(@Value("${app.system.checkImage.enabled}") boolean enableCheck,
                       @Value("${app.system.checkImage.useType}") String useType,
-                      @Value("${app.system.checkImage.imagePrivateCloudUrl}") String imagePrivateCloudUrl) {
-        this.accessKeyId = accessKeyId;
-        this.accessKeySecret = accessKeySecret;
+                      @Value("${app.system.checkImage.imagePrivateCloudUrl}") String imagePrivateCloudUrl,
+                      AliyunLib aliyunLib) {
+        this.enableCheck = enableCheck;
         this.useType = useType;
         this.imagePrivateCloudUrl = imagePrivateCloudUrl;
+        this.aliyunLib = aliyunLib;
+    }
+
+    /**
+     * byte数组转换成16进制字符串
+     *
+     * @param src
+     * @return
+     */
+    public static String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+        for (int i = 0; i < src.length; i++) {
+            int v = src[i] & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 根据文件流判断图片类型
+     *
+     * @param fis
+     * @return jpg/png/gif/bmp
+     */
+    public static String getPicType(InputStream fis) {
+        //读取文件的前几个字节来判断图片格式
+        byte[] b = new byte[4];
+        try {
+            fis.read(b, 0, b.length);
+            String type = bytesToHexString(b).toUpperCase();
+            if (type.contains("FFD8FF")) {
+                return TYPE_JPG;
+            } else if (type.contains("89504E47")) {
+                return TYPE_PNG;
+            } else if (type.contains("47494638")) {
+                return TYPE_GIF;
+            } else if (type.contains("424D")) {
+                return TYPE_BMP;
+            } else {
+                return TYPE_UNKNOWN;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 
     public void checkImageByPrivate(ByteArrayResource resource) throws DefinitionException {
+        HttpRequest request = HttpRequest.post(imagePrivateCloudUrl)
+                .timeout(20000);
+        String picType;
+        try {
+            picType = getPicType(resource.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
-            String responseStr = HttpRequest.post(imagePrivateCloudUrl)
-                    .form("image", resource.getInputStream().readAllBytes(), "pic.jpg")//表单内容
-                    .timeout(20000).execute().body();
-            ObjectMapper mapper = new ObjectMapper();
-            PrivateImageCheckerResponse response = mapper.readValue(responseStr, PrivateImageCheckerResponse.class);
-            if (response.porn > 0.5) {
-                throw new DefinitionException("可能包含违规图片，请检查后发送。");
+            if (TYPE_GIF.equals(picType)) {
+                List<ByteArrayResource> frames = gifDrawFrame(resource);
+                MultiResource multiResource = new MultiResource();
+                for (int i = 0; i < frames.size(); i++) {
+                    ByteArrayResource o = frames.get(i);
+                    multiResource.add(new BytesResource(o.getByteArray(), i + ".jpg"));
+                }
+                request.form("images", multiResource);
+            } else {
+                request.form("images", resource.getInputStream().readAllBytes(), "pic.jpg");
             }
+            String responseStr = request.execute().body();
+            ObjectMapper mapper = new ObjectMapper();
+            List<PrivateImageCheckerResponse> response = mapper.readValue(responseStr, new TypeReference<>() {
+            });
+            response.stream().filter(PrivateImageCheckerResponse::isPorn).findFirst().ifPresent(r -> {
+                throw new DefinitionException("图片审核不通过");
+            });
         } catch (IOException e) {
             throw new DefinitionException("图片检测服务异常，请通知管理员或去除图片后发送。");
         }
 
     }
 
-    public void checkImageByAliyun(String url) {
-        //请替换成你自己的accessKeyId、accessKeySecret
-        IClientProfile profile = DefaultProfile.getProfile("cn-beijing", accessKeyId, accessKeySecret);
-        DefaultProfile.addEndpoint("cn-beijing", "Green", "green.cn-beijing.aliyuncs.com");
-        IAcsClient client = new DefaultAcsClient(profile);
-
-        ImageSyncScanRequest imageSyncScanRequest = new ImageSyncScanRequest();
-        imageSyncScanRequest.setAcceptFormat(FormatType.JSON); // 指定api返回格式
-        imageSyncScanRequest.setMethod(com.aliyuncs.http.MethodType.POST); // 指定请求方法
-        imageSyncScanRequest.setEncoding("utf-8");
-        imageSyncScanRequest.setRegionId("cn-beijing");
-
-        List<Map<String, Object>> tasks = new ArrayList<Map<String, Object>>();
-        Map<String, Object> task = new LinkedHashMap<String, Object>();
-        task.put("dataId", UUID.randomUUID().toString());
-        task.put("url", url);
-        task.put("time", new Date());
-
-        tasks.add(task);
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = new HashMap<>();
-        /*
-          porn: 色情
-          terrorism: 暴恐
-          qrcode: 二维码
-          ad: 图片广告
-          ocr: 文字识别
-         */
-        map.put("scenes", Arrays.asList("porn", "terrorism"));
-        map.put("tasks", tasks);
-
-        try {
-            imageSyncScanRequest.setHttpContent(mapper.writeValueAsBytes(map), "UTF-8", FormatType.JSON);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        imageSyncScanRequest.setConnectTimeout(3000);
-        imageSyncScanRequest.setReadTimeout(10000);
-
-        try {
-            HttpResponse httpResponse = client.doAction(imageSyncScanRequest);
-
-            if (httpResponse.isSuccess()) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode scrResponse = objectMapper.readTree(httpResponse.getHttpContent());//将Json串以树状结构读入内存
-                System.out.println(scrResponse.asText());
-                if (200 == scrResponse.get("code").asInt()) {
-                    JsonNode taskResults = scrResponse.get("data");
-                    for (JsonNode taskResult : taskResults) {
-                        if (200 == taskResult.get("code").asInt()) {
-                            JsonNode sceneResults = taskResult.get("results");
-                            for (JsonNode sceneResult : sceneResults) {
-                                String scene = sceneResult.get("scene").asText();
-                                String suggestion = sceneResult.get("suggestion").asText();
-                                if (!"pass".equals(suggestion)) {
-                                    throw new DefinitionException("图片有敏感词，请检查后再试。提示：" + scene);
-                                }
-                            }
-                        } else {
-                            throw new DefinitionException("内容鉴别服务任务过程失败！code:" + taskResult.get("code").asInt());
-                        }
-                    }
-                } else {
-                    throw new DefinitionException("内容鉴别服务检测不成功！code:" + scrResponse.get("code").asInt());
-                }
-            } else {
-                throw new DefinitionException("内容鉴别服务连接异常！status:" + httpResponse.getStatus());
+    public ByteArrayResource getImage(String url) throws DefinitionException {
+        byte[] bytes = HttpUtil.downloadBytes(url);
+        ByteArrayResource resource = new ByteArrayResource(bytes) {
+            @Override
+            public String getFilename() {
+                return "image.jpg";
             }
-        } catch (ClientException | IOException e) {
-            throw new DefinitionException("内容鉴别服务连接异常！", e);
+        };
+        if (enableCheck) {
+            switch (useType) {
+                case "aliyun" -> aliyunLib.checkImage(url);
+                case "private" -> checkImageByPrivate(resource);
+            }
         }
+        return resource;
     }
 
-    public void checkImage(ByteArrayResource resource) throws DefinitionException {
+    public List<ByteArrayResource> gifDrawFrame(ByteArrayResource resource) {
+        List<ByteArrayResource> result = new ArrayList<>();
+        try {
+            GifDecoder gd = new GifDecoder();
+            //要处理的图片
+            int status = gd.read(resource.getInputStream());
+            if (status != GifDecoder.STATUS_OK) {
+                throw new RuntimeException("读取图片失败");
+            }
 
-        switch (useType) {
-//            case "aliyun" -> checkImageByAliyun(inputStreamResource);
-            case "private" -> checkImageByPrivate(resource);
+            for (int i = 0; i < gd.getFrameCount(); i += gd.getFrameCount() / 3) {
+                BufferedImage frame = gd.getFrame(i);
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                int width = frame.getWidth();
+                int height = frame.getHeight();
+                BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+                int[] px = new int[width * height];
+                frame.getRGB(0, 0, width, height, px, 0, width);
+                output.setRGB(0, 0, width, height, px, 0, width);
+                ImageIO.write(output, "jpg", outStream);
+                result.add(new ByteArrayResource(outStream.toByteArray()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
+        return result;
     }
 
     @Data
@@ -147,5 +191,9 @@ public class ImageTools {
         private Double drawing;
         private Double hentai;
 
+        public boolean isPorn() {
+            return porn > 0.5;
+        }
     }
+
 }
